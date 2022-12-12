c33dbb35e225b4f096999c9a042a3b54e72a6d46
Raj Changappa <rajchangappa1@gmail.com>
2022-12-12 18:01:53 +0530

    changes

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 // QUESTIONS
 const QUESTIONS = [
 	{
-		id: 1,
+		id: 0,
 		q: "What was Meta Platforms Inc formerly known as?",
-		op1: "IBM",
-		op2: "Facebook",
-		op3: "Google",
-		op4: "Infosys",
-		answer: "Facebook",
+		a: [
+			{ op1: "IBM" },
+			{ op2: "Google" },
+			{ op3: "Infosys" },
+			{ op4: "Facebook" },
+			{ isCorrect: "Facebook" },
+		],
+	},
+	{
+		id: 1,
+		q: "Which English city is known as the Steel City?",
+		a: [
+			{ op1: "Sheffield" },
+			{ op2: "Manchester" },
+			{ op3: "London" },
+			{ op4: "Nottingham" },
+			{ isCorrect: "Sheffield" },
+		],
 	},
 	{
 		id: 2,
-		q: "Which English city is known as the Steel City?",
-		op1: "Sheffield",
-		op2: "Manchester",
-		op3: "London",
-		op4: "Nottingham",
-		answer: "Sheffield",
+		q: "The logo for luxury car maker Porsche features which animal?",
+		a: [
+			{ op1: "Jaguar" },
+			{ op2: "Leopard" },
+			{ op3: "Horse" },
+			{ op4: "Falcon" },
+			{ isCorrect: "Horse" },
+		],
 	},
 	{
 		id: 3,
-		q: "The logo for luxury car maker Porsche features which animal?",
-		op1: "Jaguar",
-		op2: "Leopard",
-		op3: "Horse",
-		op4: "Falcon",
-		answer: "Horse",
-	},
-	{
-		id: 4,
 		q: "What is a young giraffe called?",
-		op1: "Calf",
-		op2: "Mare",
-		op3: "Joey",
-		op4: "ram",
-		answer: "Calf",
+		a: [
+			{ op1: "Calf" },
+			{ op2: "Mare" },
+			{ op3: "Joey" },
+			{ op4: "ram" },
+			{ isCorrect: "Calf" },
+		],
 	},
 ];
 
@@ -46,76 +54,87 @@ const option3Text = document.querySelector("#option3");
 const option4Text = document.querySelector("#option4");
 const scoreText = document.querySelector("#score");
 const totalText = document.querySelector("#total");
-const nextBtn = document.querySelector("#next-btn");
 
 let score = 0;
+totalText.textContent = `Total Points : ${QUESTIONS.length}`;
+// ITERATES THROUGH QUESTIONS
+function iterate(id) {
+	// populate question text
+	questionText.textContent = QUESTIONS[id].q;
 
-// START QUIZ
-function start() {
-	generateQuestion();
+	// populate option text
+	option1Text.textContent = QUESTIONS[id].a[0].op1;
+	option2Text.textContent = QUESTIONS[id].a[1].op2;
+	option3Text.textContent = QUESTIONS[id].a[2].op3;
+	option4Text.textContent = QUESTIONS[id].a[3].op4;
+
+	getUserSelect();
+
+	// get user selection
+	function getUserSelect() {
+		let selected;
+
+		option1Text.addEventListener("click", () => {
+			option1Text.classList.add("selected");
+			selected = option1Text.textContent;
+			check(selected);
+		});
+
+		option2Text.addEventListener("click", () => {
+			option2Text.classList.add("selected");
+			selected = option2Text.textContent;
+			check(selected);
+		});
+
+		option3Text.addEventListener("click", () => {
+			option3Text.classList.add("selected");
+			selected = option3Text.textContent;
+			check(selected);
+		});
+
+		option4Text.addEventListener("click", () => {
+			option4Text.classList.add("selected");
+			selected = option4Text.textContent;
+			check(selected);
+		});
+	}
+
+	// console.log(score);
+
+	// evaluate options
+	function check(selected) {
+		if (selected == QUESTIONS[id].a[4].isCorrect && score < QUESTIONS.length) {
+			score++;
+			console.log(score);
+			scoreText.textContent = `Score: ${score}`;
+			removeSelection();
+		} else {
+			scoreText.textContent = `Score: ${score}`;
+			removeSelection();
+		}
+	}
+
+	// remove selections
+	function removeSelection() {
+		setInterval(() => {
+			option1Text.classList.remove("selected");
+			option2Text.classList.remove("selected");
+			option3Text.classList.remove("selected");
+			option4Text.classList.remove("selected");
+		}, 1000);
+
+		loadNext();
+	}
+
+	// LOAD NEXT QUESTION
+	function loadNext() {
+		setTimeout(() => {
+			if (id < QUESTIONS.length) {
+				QUESTIONS[id] = id++;
+				iterate(id);
+			}
+		}, 500);
+	}
 }
 
-// GENERATE QUESTION AND POPULATE OPTIONS
-function generateQuestion(id) {
-	console.log(QUESTIONS);
-	questionText.textContent = question.q;
-
-	option1Text.textContent = question.op1;
-	option2Text.textContent = question.op2;
-	option3Text.textContent = question.op3;
-	option4Text.textContent = question.op4;
-
-	getUserSelection(question);
-}
-
-// GET USER SELECTION
-function getUserSelection(question) {
-	option1Text.addEventListener("click", (e) => {
-		if (option1Text.textContent == question.answer) {
-			option1Text.classList.add("correct");
-			score++;
-			scoreText.textContent = `Score: ${score}`;
-		} else {
-			option1Text.classList.add("incorrect");
-			return;
-		}
-	});
-
-	option2Text.addEventListener("click", (e) => {
-		if (option2Text.textContent == question.answer) {
-			option2Text.classList.add("correct");
-			score++;
-			scoreText.textContent = `Score: ${score}`;
-		} else {
-			option2Text.classList.add("incorrect");
-			return;
-		}
-	});
-
-	option3Text.addEventListener("click", (e) => {
-		if (option3Text.textContent == question.answer) {
-			option3Text.classList.add("correct");
-			score++;
-			scoreText.textContent = `Score: ${score}`;
-		} else {
-			option3Text.classList.add("incorrect");
-			return;
-		}
-	});
-
-	option4Text.addEventListener("click", (e) => {
-		if (option4Text.textContent == question.answer) {
-			option4Text.classList.add("correct");
-			score++;
-			scoreText.textContent = `Score: ${score}`;
-		} else {
-			option4Text.classList.add("incorrect");
-			return;
-		}
-	});
-}
-
-// GENERATE NEXT QUESTION
-nextBtn.addEventListener("click", generateQuestion);
-
-start();
+iterate(0);
